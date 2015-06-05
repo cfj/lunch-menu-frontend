28af87c07056feddeb48b1b88083e1f646d58bcd
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2015-06-05 13:24:41 +0200

    Add url to Heroku

--- app/scripts/services/menuservice.js
+++ app/scripts/services/menuservice.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('lunchFrontendApp').factory('menuService', function ($http) {
-    var apiUrl = 'http://localhost:8080'
+    var apiUrl = 'https://mysterious-wave-8770.herokuapp.com'
 
     return {
         getMenus: function () {

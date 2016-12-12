60118bfcaf58b3d49960e0f69ceb552146b56995
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2016-12-12 12:52:43 +0100

    Cache busting

--- app/scripts/services/menuservice.js
+++ app/scripts/services/menuservice.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('lunchFrontendApp').factory('menuService', function ($http) {
-    var jsonUrl = 'https://s3.eu-central-1.amazonaws.com/lunch-menu-api.cfj/menus.json';
+    var jsonUrl = 'https://s3.eu-central-1.amazonaws.com/lunch-menu-api.cfj/menus.json?v=2';
 
     return {
         getMenus: function () {
